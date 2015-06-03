8c596665f5836157fe5cb9cc76212b0657998409
IgorWang <125942418@qq.com>
2015-06-03 16:45:24 +0800

    database settings

--- MyBlogSite/settings.py
+++ MyBlogSite/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'MyBlogSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myblogdb',
+        'USER': 'root',
+        'PASSWORD':'password',
     }
 }
 
@@ -87,7 +89,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = ''
 
 USE_I18N = True
 
