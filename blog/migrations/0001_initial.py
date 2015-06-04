dd8459cbc8ad53605e431d5ba285716088aabd73
Igor <125942418@qq.com>
2015-06-04 20:37:46 +0800

    add blog app

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Blog',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('title', models.CharField(max_length=200)),
+                ('body', models.TextField()),
+                ('slug', models.SlugField(max_length=200, unique=True)),
+                ('publish', models.BooleanField(default=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now_add=True)),
+            ],
+            options={
+                'verbose_name': 'Blog Entry',
+                'ordering': ['-created'],
+                'verbose_name_plural': 'Blog Entries',
+            },
+        ),
+    ]
