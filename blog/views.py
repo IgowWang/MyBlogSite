fc5a784ca27b33437ddb1e073c72e6478fdd7c4b
IgorWang <125942418@qq.com>
2015-06-22 11:23:46 +0800

    data qureyset

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,11 @@ from django.views import generic
 # Create your views here.
 
 def blog_index(request):
+    all_blogs = models.Articles.objects.all().order_by('-publish_time')
 
-    return render(request,"bloghome.html",{})
+    context={
+        "blogs" : all_blogs
+    }
+
+
+    return render(request,"bloghome.html",context)
